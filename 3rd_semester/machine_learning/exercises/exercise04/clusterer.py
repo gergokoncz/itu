a05cdddd7820dec5989a680b688682e1fad9412d
Gergo Koncz <gergokoncz06@gmail.com>
2019-09-22 16:48:38 +0200

    k-means classifier runs for all data

--- 3rd_semester/machine_learning/exercises/exercise04/clusterer.py
+++ 3rd_semester/machine_learning/exercises/exercise04/clusterer.py
@@ -9,10 +9,55 @@ def iris_reader():
 
 def clusterer(input_data, number_of_clusters):
     np.random.seed(125)
+
+    # initial values for cluster centers
     max_values = np.amax(input_data, axis = 0).tolist()
     min_values = np.amin(input_data, axis = 0).tolist()
     m_s = [[np.random.uniform(low = min_value, high = max_values[idx]) for idx, min_value in enumerate(min_values)] for i in range(number_of_clusters)]
-    print(m_s)
+    cluster_center_printer(m_s)
+
+    # run the whole optimization 
+    n_of_cycles = int(input("give me number of cycles: "))
+    for i in range(n_of_cycles):
+        # expectaion round
+        labels = []
+        for row in input_data:
+            label = 0
+            min_distance = 1000
+            for idx,m in enumerate(m_s):
+                distance = sum([(m[i] - row[i]) ** 2 for i in range(len(row))])
+            #    print(distance, idx)
+                if distance < min_distance:
+                    label = idx
+                    min_distance = distance
+            labels.append(label)
+            #print(row, min_distance, label)
+
+        # maximization round
+        for i in range(len(m_s)):
+            sums = [0 for m in m_s[i]]
+            n_of_elements = 0
+            for idx,label in enumerate(labels):
+                if label == i:
+                    n_of_elements += 1
+                    for this_id,value in enumerate(sums):
+                        #print(value, input_data[idx, this_id])
+                        sums[this_id] += input_data[idx, this_id]
+            print(f"cluster {label} has {n_of_elements} elements")
+            m_s[i] = [this_sum / n_of_elements for this_sum in sums]
+        
+        cluster_center_printer(m_s)
+    
+    # to check labels
+    with open("my_labels.txt", "w") as this_file:
+        for label in labels:
+            this_file.write(f"{label}\n")
+
+def cluster_center_printer(cluster_center):
+    cluster_center = [[str(x) for x in row] for row in cluster_center]
+    for idx,row in enumerate(cluster_center):
+        print(f"cluster_id: {idx}")
+        print("\t".join(row))
 
 if __name__ == "__main__":
     all_data = iris_reader()
