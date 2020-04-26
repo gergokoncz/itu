61c5e0f3fbd5a0bb4990aa73c6049c1b23829c3e
gergokoncz <gergokoncz06@gmail.com>
2020-04-26 15:17:34 +0200

    model iterations

--- 4th_semester/large_scale_data/assignment4/model_implementation.py
+++ 4th_semester/large_scale_data/assignment4/model_implementation.py
@@ -4,8 +4,9 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from datetime import datetime, timedelta
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 
 
 class DateTransformer(BaseEstimator, TransformerMixin):
@@ -49,7 +50,7 @@ if __name__ == '__main__':
     client = InfluxDBClient(host = 'influxus.itu.dk', port = 8086, username = 'lsda', password = 'icanonlyread')
     client.switch_database('orkney')
 
-    results = client.query('SELECT * FROM "Generation" where time > now() - 4w ORDER BY time')
+    results = client.query('SELECT * FROM "Generation" where time > now() - 8w ORDER BY time')
     points = results.get_points()
     values = results.raw['series'][0]["values"]
     columns = results.raw['series'][0]["columns"]
@@ -62,6 +63,11 @@ if __name__ == '__main__':
 
     processed_data = preparation_pipeline.fit_transform(generation_df)
 
-#    date_transformed, target = DateTransformer().fit_transform(generation_df)
-#    shifted_transformed, target = Shifter().fit_transform(date_transformed, y = target)
-    print(processed_data)
+    dim1, dim2 = processed_data[0].shape
+
+
+    lreg_model = Lasso().fit(processed_data[0].iloc[:int(0.8*dim1)], processed_data[1][:int(0.8 * dim1)])
+    print(lreg_model.score(processed_data[0].loc[int(0.8 * dim1):], processed_data[1][int(0.8 * dim1):]))
+
+    predictions = lreg_model.predict()
+
