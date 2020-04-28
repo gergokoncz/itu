84258f9358d83b14ba24b608aaec117fc9aba8a1
Gergo Koncz <gergokoncz06@gmail.com>
2020-04-28 16:20:43 +0200

    model started

---
+++ 4th_semester/large_scale_data/assignment4/energy_gen_forecast/mytransformers.py
@@ -0,0 +1,37 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class DateTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        None
+    
+    def fit(self, X, y = None):
+        return self
+
+    def transform(self, X, y = None):
+        X = X.reset_index()
+        X['datetime'] = X['time'].apply(lambda x: datetime.strptime(x[:19], '%Y-%m-%dT%H:%M:%S'))
+        X['year'] = X['datetime'].apply(lambda x: x.year)
+        X['month'] = X['datetime'].apply(lambda x: x.month)
+        X['day'] = X['datetime'].apply(lambda x: x.day)
+        X['hour'] = X['datetime'].apply(lambda x: x.hour)
+        X['minute'] = X['datetime'].apply(lambda x: x.minute)
+        X['day_of_week'] = X['datetime'].apply(lambda x: x.dayofweek)
+        y = X['Total']
+        return X[['year', 'month', 'day', 'hour', 'minute', 'day_of_week', 'Total']], y
+
+class Shifter(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        None
+    
+    def fit(self, X, y = None):
+        return self
+
+    def transform(self, X, y = None, hours = 3):
+        X = X[0]
+        for i in range(hours):
+            colname = f"{i+1}hourback"
+            X[colname] = X['Total'].shift(60 * (-i-1))
+        X = X.dropna()
+        y = X['Total']
+        X = X.drop(columns = ['Total'], axis = 1)
+        return X, y
