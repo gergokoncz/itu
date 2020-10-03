652e2e770743ee6c3d21a01c10f7ffd66c74639d
gergokoncz <gergokoncz06@gmail.com>
2020-10-03 16:28:11 +0200

    static demo

---
+++ 5th_semester/software_development/source/staticDemo.java
@@ -0,0 +1,21 @@
+public class staticDemo {
+    public static void main(String args[]) {
+        Student s1 = new Student();
+        s1.showData();
+        Student s2 = new Student();
+        s2.showData();
+    }
+}
+
+class Student {
+    int a;
+    static int b;
+
+    Student() {
+        b++;
+    }
+    public void showData() {
+        System.out.println("value of a = " + a);
+        System.out.println("value of b = " + b);
+    }
+}
