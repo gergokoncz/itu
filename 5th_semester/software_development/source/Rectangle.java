f378adeda5b3efaa496fc62ba2a21041b79e4307
gergokoncz <gergokoncz06@gmail.com>
2020-10-04 10:56:38 +0200

    final demo and Makefile for cleaning

---
+++ 5th_semester/software_development/source/Rectangle.java
@@ -0,0 +1,15 @@
+abstract class Shape {
+    final int b = 20;
+    public void display() {
+        System.out.println("This is display method");
+    }
+    abstract public void calculateArea();
+}
+
+public class Rectangle extends Shape {
+    public static void main (String args[]) {
+        Rectangle obj = new Rectangle();
+        obj.display();
+    }
+    public void calculateArea(){}
+}
