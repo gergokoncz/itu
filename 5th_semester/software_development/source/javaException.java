1c538334e0d4f2e434fcc744e8598b8cf08e9324
gergokoncz <gergokoncz06@gmail.com>
2020-10-09 00:31:02 +0200

    exceptions

---
+++ 5th_semester/software_development/source/javaException.java
@@ -0,0 +1,21 @@
+class javaException {
+    public static void main(String args[]) {
+        try {
+            int d = 1;
+            int n = 20;
+            int fraction = n / d;
+            int g[] = {1};
+            g[20] = 100;
+        }
+        //catch(Exception e){
+        //    System.out.println("In the catch block due to Exception = " + e);
+        //}
+        catch(ArithmeticException e) {
+            System.out.println("In the catch block due to Exception = " + e);
+        }
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("In the catch block due to Exception = " + e);
+        }
+        System.out.println("End of Main");
+    }
+}
